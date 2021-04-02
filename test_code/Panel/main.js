917740f93fe9630a6d71b03c5783c9d478c87ae9
unknown <travisbostick@gmail.com>
2021-04-02 01:08:49 -0500

    messing around with markdown

---
+++ test_code/Panel/main.js
@@ -0,0 +1,22 @@
+var converter = new showdown.Converter(),
+    text = '# hello, this text is a markdown string!',
+    html= converter.makeHtml(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d3.select('#markDiv').html(html);
+
+
+
+
